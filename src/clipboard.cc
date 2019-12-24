a1b586ce4460af631799f5c66bbe1fdabf755bbe
Smiley32 <jeannin.emile@gmail.com>
2019-12-24 15:56:29 +0200

    Better code to write paths in the clipboard

--- src/clipboard.cc
+++ src/clipboard.cc
@@ -7,20 +7,16 @@
 
 using namespace v8;
 
+/**
+ * Write the given paths in the clipboard (Windows)
+ * 
+ * @param sFiles - Array of char containing every paths, separated by '\0'
+ * @param nLen - Length of the char array
+ */
 void writePathsInClipboard (char * sFiles, int nLen) {
-    // char sFiles[] = "D:\\Alita Battle Angel AC3 Dubbed 1080p\0";
-	//	"C:\\Users\\Smiley32\\Documents\\Godot\\2048\\2048.exe";
 	DROPFILES dobj = { 20, { 0, 0 }, 0, 1 };
 
-    for(int i = 0; i < nLen; i++) {
-        if (sFiles[i] == '\0') {
-            continue;
-        }
-        std::cout << sFiles[i];
-    }
-    std::cout << std::endl;
-
-	int nGblLen = sizeof(dobj) + nLen * 2 + 5;//lots of nulls and multibyte_char
+	int nGblLen = sizeof(dobj) + nLen * 2 + 5;
 	HGLOBAL hGbl = GlobalAlloc(GMEM_ZEROINIT | GMEM_MOVEABLE | GMEM_DDESHARE, nGblLen);
 	char* sData = (char*)::GlobalLock(hGbl);
 	memcpy(sData, &dobj, 20);
@@ -38,23 +34,16 @@ void writePathsInClipboard (char * sFiles, int nLen) {
 	}
 }
 
+/**
+ * Method called by javascript
+ * Will write each path given in the clipboard
+ * An array of strings must be sent
+ */
 NAN_METHOD(writeFiles) {
     Isolate        *isolate     = info.GetIsolate();
     Local<Context>  context     = isolate->GetCurrentContext();
 
-    // MaybeLocal<String> maybe_str = info[0]->ToString(context);
-    MaybeLocal<Object> maybeArray = info[0]->ToObject(context);
-
-    if (maybeArray.IsEmpty()) {
-        std::cout << "YEEES" << std::endl;
-        return;
-    }
-    std::cout << "WUT?" << std::endl;
-
-    Local<Object> array = maybeArray.ToLocalChecked();
     Local<Array> test = Local<Array>::Cast(info[0]);
-
-    std::cout << test->Length() << " ?!?" << std::endl;
 
     char * sFiles = NULL;
     int totalSize = 0;
@@ -66,45 +55,17 @@ NAN_METHOD(writeFiles) {
 
         int prevSize = totalSize;
         totalSize += path.length() + 1;
-        std::cout << "ICI?" << std::endl;
+        // path length + \0
         sFiles = (char*)realloc(sFiles, totalSize);
         
-
+        // copy with \0
         std::strcpy(sFiles + prevSize, *path);
-
-        // std::cout << "[" << i << "]: " << std::string(*path) << std::endl;
-        /*
-        pathArray[i] = new char[path.length() + 1];
-        std::strcpy(pathArray[i], *path);
-
-        std::cout << "ici " << *path << " -:- " << path.length() << std::endl;
-        */
     }
     
-    std::cout << "chaines: ";
-    for (int iChar = 0; iChar < totalSize; iChar++) {
-        std::cout << sFiles[iChar];
-    }
-    std::cout << std::endl;
-
     writePathsInClipboard(sFiles, totalSize);
 
     free(sFiles);
-
-    /*
-    for (int i = 0; i < test->Length(); i++) {
-        delete[] pathArray[i];
-    }
-    delete[] pathArray;
-    */
 }
-
-/*
-NAN_MODULE_INIT(InitModule) {
-  NODE_SET_METHOD(target, "writeFiles", writeFiles);
-}
-*/
-
 
 NAN_MODULE_INIT(Init) {
     Nan::Set(target,
